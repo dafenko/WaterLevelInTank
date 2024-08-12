f988484006bf776c6f5db283dcf529f2245e7941
Daniel Ferenci <daniel.ferenci@gmail.com>
2024-08-12 22:25:02 +0200

    Even smoother with EMA

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,34 +1,46 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-//  Created and Designed by Manusha Ramanayake 
-//  Email   : ramanayakemanusha@gmail.com
+// Constants and Variables
+SoftwareSerial HC12(4, 5);   // HC-12 TX Pin 4, RX Pin 5
 
-
-//Transmitter  and Sensor Unit
-
-# include <SoftwareSerial.h>
-// # include "avr8-stub.h"
-# include "app_api.h"
-
-SoftwareSerial HC12(4, 5);
-
+const int trig = 3;          // Trigger pin of ultrasonic sensor
+const int echo = 2;          // Echo pin of ultrasonic sensor
+const int senzor_id = 1;
+const int height_of_tank = 200;
+const float alpha = 0.1;     // EMA Smoothing factor (between 0 and 1)
 long distance_old = 0;
 int Time = 0;
-int trig = 3; //trigger pin of ultrasonic sensor
-int echo = 2; //Echo pin of ultrasonic sensor
-int senzor_id = 1;
-int height_of_tank = 200;
-int OUTLIER_THRESHOLD=5;
-int INLIER_THRESHOLD=5;
+float ema_value = 0;         // Initial EMA value
 
 void setup() {
-//  debug_init();
-  //To calibrate the  sensor delete the comment symbols in the following code
-  Serial.begin(9600);
-  HC12.begin(9600);
-  pinMode(trig, OUTPUT);
+  Serial.begin(9600);        // Initialize Serial communication at 9600 baud
+  HC12.begin(9600);          // Initialize HC-12 communication at 9600 baud
+  pinMode(trig, OUTPUT);     
+  digitalWrite(trig, LOW);   // Set trigger pin to LOW
+  pinMode(echo, INPUT);      // Set echo pin as input
+}
+
+float getEMA(int newReading, float prevEMA, float alpha) {
+  return alpha * newReading + (1 - alpha) * prevEMA;
+}
+
+int getDistance() {
+  digitalWrite(trig, LOW); 
+  delayMicroseconds(5);
+  digitalWrite(trig, HIGH); 
+  delayMicroseconds(10);
   digitalWrite(trig, LOW);
-  pinMode(echo,  INPUT);
+  
+  long duration = pulseIn(echo, HIGH, 30000);  // Timeout after 30ms
+  int distance = (duration * 0.034) / 2;       // Convert duration to distance (cm)
+
+  // Return distance only if it's within a reasonable range
+  if (distance > 0 && distance < height_of_tank) {
+    return distance;
+  } else {
+    return height_of_tank; // Return a maximum value if out of range
+  }
 }
 
 void Transmit_data(int senzor_id, int distance) {
@@ -39,59 +51,25 @@ void Transmit_data(int senzor_id, int distance) {
 }
 
 void Send_data(int distance) {
-
-   //This sends data every 100 ms
-  if (Time > 1000) {
-    Serial.println("Sending because time");
+  // Send data every 100 ms or instantly if the distance changes
+  if (Time > 1000 || distance != distance_old) {
     Transmit_data(senzor_id, distance);
     Time = 0;
-
-    return;
-  }
-
-  //This sends data instantly when tank water level is changing
-  if (distance != distance_old) {   
-    Transmit_data(senzor_id, distance);
     distance_old = distance;
   }
-
-  return;
 }
 
-int getDistance() {
-  // Standerd code for getting value from the ultrasonic  sensor
-  digitalWrite(trig, LOW); 
-  delayMicroseconds(5);
-  digitalWrite(trig,  HIGH); 
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  
-  return(pulseIn(echo, HIGH, 30000)*0.034/2);   //this gets the value in centimeters
-}
-
-int getAverageDistance(int numSamples) {
-  long sum = 0;
-  for (int i = 0; i < numSamples; i++) {
-    int distance = getDistance();
-    sum += distance;
-    delay(50);  // Small delay between readings
-  }
-  return sum / numSamples;
-}
-
-void  loop() {
+void loop() {
   Time++;
-
-  int distance = getAverageDistance(30);
-  if ((0 < distance) && (distance < height_of_tank)) {
-    //Serial.println(distance);
-    Send_data(distance);
-  }
-  else 
-  {
-    //Serial.println("Wrong reading " + String(distance));
-    distance_old = distance;
-  }
   
-} 
+  int raw_distance = getDistance();         // Get raw distance from the sensor
+  ema_value = getEMA(raw_distance, ema_value, alpha);  // Apply EMA filter
 
+  int filtered_distance = round(ema_value); // Round EMA to the nearest integer
+
+  if (filtered_distance > 0 && filtered_distance < height_of_tank) {
+    Send_data(filtered_distance);           // Send the filtered distance data
+  }
+
+  delay(100);  // Adjust as needed
+}
