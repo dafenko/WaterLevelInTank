330835f57aa17b01b35cad4b306f9c55f4d89601
Daniel Ferenci <daniel.ferenci@gmail.com>
2024-10-17 00:17:25 +0200

    working version with sleep

--- ATTiny85/src/main.cpp
+++ ATTiny85/src/main.cpp
@@ -1,74 +1,61 @@
-#include <SoftwareSerial.h>
+#include <Arduino.h>
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/wdt.h>
+#include <SoftwareSerial.h>
 
-// Pin definitions for ATtiny85
-const int ult_sensonr_rx = 0;  // Pin 0 for ultrasonic sensor RX
-const int ult_sensor_tx = 1;   // Pin 1 for ultrasonic sensor TX
-const int power_radio = 3;     // Pin 3 for power control of the HC-12
 
-// Constants and Variables
-SoftwareSerial HC12(2, 3);   // HC-12 TX on Pin 2, RX on Pin 3
-SoftwareSerial jsnSerial(ult_sensonr_rx, ult_sensor_tx);
+byte saveADCSRA;                      // variable to save the content of the ADC for later. if needed.
+volatile byte counterWD = 0;            // Count how many times WDog has fired. Used in the timing of the 
+                                           // loop to increase the delay before the LED is illuminated. For example,
+                                        // if WDog is set to 1 second TimeOut, and the counterWD loop to 10, the delay
+                                        // between LED illuminations is increased to 1 x 10 = 10 seconds
 
+// device specific
 const int senzor_id = 1;
 const int height_of_tank = 200;
-const float alpha = 0.1;     // EMA Smoothing factor
-long distance_old = 0;
-float ema_value = 0;         // Initial EMA value
 
-volatile int f_wdt = 1;
-volatile int sleep_counts = 0;
-const int max_sleep_rounds = 3;
-const int max_measurements = 5;
+const int hc12_tx = PB1;
+const int hc12_rx = PB0;
+const int ult_sensonr_rx = PB3;  // Pin 0 for ultrasonic sensor RX
+const int ult_sensor_tx = PB4;   // Pin 1 for ultrasonic sensor TX
+const int power_radio = PB2; 
 const int max_transmit_loops = 10;
+const int max_sleep_rounds = 9;
 
-// Watchdog Interrupt Service Routine (ISR)
-ISR(WDT_vect) {
-  if (f_wdt == 0) {
-    sleep_counts++;
-    if (sleep_counts >= max_sleep_rounds) {
-      f_wdt = 1;
-      sleep_counts = 0;
-    }
-  }
-}
+SoftwareSerial HC12(hc12_tx, hc12_rx);   // HC-12 TX on Pin 2, RX on Pin 3
+SoftwareSerial jsnSerial(ult_sensonr_rx, ult_sensor_tx);
 
-// Enter ATtiny85 into sleep mode
-void enterSleep(void) {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Deep sleep mode
-  sleep_enable();
-  sleep_mode(); // Go to sleep
-  sleep_disable(); // Wake up
-  power_all_enable(); // Re-enable peripherals
-}
-
-// Setup Watchdog Timer (WDT)
-void setupWatchDogTimer() {
-  MCUSR &= ~(1 << WDRF); // Clear reset flag
-  WDTCR |= (1 << WDCE) | (1 << WDE); // Set WDCE and WDE
-  WDTCR = (1 << WDP2) | (1 << WDP1) | (1 << WDP0); // Set WDT to 8 seconds timeout
-  WDTCR |= _BV(WDIE); // Enable WDT interrupt
-}
-
-void powerOnDevices() {
-  // Turn on radio module
-  pinMode(power_radio, OUTPUT);
-  digitalWrite(power_radio, HIGH);
-}
-
-void setup() {
-  // ATtiny85 doesn't have hardware serial, so we're using SoftwareSerial
-  HC12.begin(9600);   // HC-12 at 9600 baud
-  jsnSerial.begin(9600); // Ultrasonic sensor at 9600 baud
+void resetWatchDog ()
+{
+  MCUSR = 0;
+  WDTCR = bit ( WDCE ) | bit ( WDE ) | bit ( WDIF ); // allow changes, disable reset, clear existing interrupt
+  WDTCR = bit ( WDIE ) | bit ( WDP2 )| bit ( WDP1 ); // set WDIE ( Interrupt only, no Reset ) and 1 second TimeOut
+                                                     
+  wdt_reset ();                            // reset WDog to parameters
   
-  setupWatchDogTimer();
-}
+} // end of resetWatchDog ()
 
-float getEMA(int newReading, float prevEMA, float alpha) {
-  return alpha * newReading + (1 - alpha) * prevEMA;
-}
+void sleepNow ()
+{
+  set_sleep_mode ( SLEEP_MODE_PWR_DOWN ); // set sleep mode Power Down
+  saveADCSRA = ADCSRA;                    // save the state of the ADC. We can either restore it or leave it turned off.
+  ADCSRA = 0;                             // turn off the ADC
+  power_all_disable ();                   // turn power off to ADC, TIMER 1 and 2, Serial Interface
+  
+  noInterrupts ();                        // turn off interrupts as a precaution
+  resetWatchDog ();                       // reset the WatchDog before beddy bies
+  sleep_enable ();                        // allows the system to be commanded to sleep
+  interrupts ();                          // turn on interrupts
+  
+  sleep_cpu ();                           // send the system to sleep, night night!
+
+  sleep_disable ();                       // after ISR fires, return to here and disable sleep
+  power_all_enable ();                    // turn on power to ADC, TIMER1 and 2, Serial Interface
+  
+  // ADCSRA = saveADCSRA;                 // turn on and restore the ADC if needed. Commented out, not needed.
+  
+} // end of sleepNow ()
 
 unsigned int getDistance() {
   unsigned int distance;
@@ -122,16 +109,37 @@ void measureAndTransmit() {
     }
   }
 }
+void setup ()
+{
+  resetWatchDog ();                     // do this first in case WDog fires
+  pinMode ( power_radio, OUTPUT );           // I could put to INPUT between sleep_enable() and interrupts()
+                                        // to save more power, then to OUTPUT in the ISR after wdt_disable()
+  HC12.begin(9600);   // HC-12 at 9600 baud
+  jsnSerial.begin(9600); // Ultrasonic sensor at 9600 baud
 
-void loop() {
-  if (f_wdt != 1) {
-    return; // Wait for WDT wakeup
-  }
+} // end of setup
 
-  f_wdt = 0; // Clear the flag for the next cycle
+void loop ()
+{
+  if ( counterWD == max_sleep_rounds )                 // if the WDog has fired max_sleep_rounds times......
+  {
+    digitalWrite ( power_radio, HIGH );        // flash the led on for 100ms
+    measureAndTransmit();                      // measure and transmit
+    digitalWrite ( power_radio, LOW );
+    counterWD = 0;                        // reset the counterWD for another 10 WDog firings
+  } // end of if counterWD
+  
+  sleepNow();                          // then set up and enter sleep mode
+  
+} // end of loop ()
 
-  enterSleep(); // Go to sleep and wake up via WDT
 
-  delay(100); // Delay before measurement
-  measureAndTransmit();
-}
+ISR ( WDT_vect )
+{
+  wdt_disable ();                           // until next time....
+  counterWD ++;                             // increase the WDog firing counter. Used in the loop to time the flash
+                                            // interval of the LED. If you only want the WDog to fire within the normal 
+                                            // presets, say 2 seconds, then comment out this command and also the associated
+                                            // commands in the if ( counterWD..... ) loop, except the 2 digitalWrites and the
+                                            // delay () commands.
+} // end of ISR
